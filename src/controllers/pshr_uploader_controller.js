1c5d0fee97daea6d5aa13e383cd9ae95e78a5466
R4ttlesnake <mail@pbernhard.com>
2019-08-17 18:58:43 +0200

    reworked uploading flow / added sortable uploads to upload panel

--- src/controllers/pshr_uploader_controller.js
+++ src/controllers/pshr_uploader_controller.js
@@ -13,6 +13,8 @@ export default class extends Controller {
   connect() {
     this.form = this.element.closest('form')
     this.formSubmit = this.form.querySelector("input[type='submit']")
+    // create info element
+    this.element.insertAdjacentHTML("beforeend", "<div class='pshr-uploader__informer' data-target='pshr-uploader.informer'></div>")
 
     this.uppy = Uppy({
         id: this.element.id,
@@ -22,6 +24,10 @@ export default class extends Controller {
           maxNumberOfFiles: this.maxNumberOfFiles,
           allowedFileTypes: this.whitelist,
           maxFileSize: this.maxFileSize
+        },
+        onBeforeFileAdded: (currentFile, files) => {
+          // reset uppy before a file is added
+          this.reset()
         }
       })
       .use(DragDrop, {
@@ -44,12 +50,13 @@ export default class extends Controller {
 
     // update progress bar
     this.uppy.on('upload-progress', (file, progress) => {
-      this.progress(file, progress)
+      this.progressHandler(file, progress)
     })
 
     // single upload succeeded
     // update file field for form and preview uploaded file
     this.uppy.on('upload-success', (file, response) => {
+      // create hash for uploaded file
       const fileData = JSON.stringify({
         id: response.uploadURL,
         storage: "cache",
@@ -60,6 +67,7 @@ export default class extends Controller {
         }
       })
 
+      // set form file target value to uploaded file hash
       this.fileTarget.value = fileData
 
       // TODO enable automatic form submission for uploader
@@ -79,18 +87,29 @@ export default class extends Controller {
     })
   }
 
-  // update progress with filename, percentage
-  // and ASCII progress bar
-  progress(file, progress) {
+  // update progress with filename, percentage,
+  // uploaded MB and ASCII progress bar
+  progressHandler(file, progress) {
     const filename = file.name
     const percentage = parseFloat(progress.bytesUploaded / progress.bytesTotal * 100).toFixed(2)
+    const sizeUploaded = (progress.bytesUploaded / 1024 / 1024).toFixed(3)
+    const sizeTotal = (progress.bytesUploaded / 1024 / 1024).toFixed(3)
 
     const bar = document.createElement('div')
-    bar.innerHTML = "#".repeat(200)
+    bar.innerHTML = "~=".repeat(200)
     bar.style.cssText = `width: ${percentage}%; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;`
 
-    this.progressTarget.innerHTML = `<strong>${percentage}%</strong> uploading ${file.id}`
-    this.progressTarget.appendChild(bar)
+    // create progress target if it doesn't exist
+    if (!this.hasProgressTarget) {
+      this.element.insertAdjacentHTML("beforeend", "<div class='pshr-uploader__progress' data-target='pshr-uploader.progress'></div>")
+    }
+
+    if (parseInt(percentage) == 100) {
+      this.progressTarget.innerHTML = `Uploaded ${file.id}`
+    } else {
+      this.progressTarget.innerHTML = `${sizeUploaded}&thinsp;/&thinsp;${sizeTotal} MB, ${percentage}% uploading ${file.id}`
+      this.progressTarget.appendChild(bar)
+    }
   }
 
   // preview the uploaded file in thumb
@@ -114,8 +133,9 @@ export default class extends Controller {
 
   // reset the uppy instance and progress bar
   reset() {
-    this.progressTarget.innerHTML = ""
-    this.thumbTarget.innerHTML = "<span>Upload new file…</span>"
+    if (this.hasProgressTarget) {
+      this.progressTarget.parentNode.removeChild(this.progressTarget)
+    }
     this.uppy.reset()
   }
 
---
+++ src/controllers/pshr_uploads_controller.js
@@ -0,0 +1,77 @@
+import { Controller } from "stimulus"
+import Sortable from "sortablejs"
+
+export default class extends Controller {
+
+  static targets = ["uploader", "uploads"]
+
+  connect() {
+    // init sortable uploads
+    this.initSortable()
+  }
+
+  // add form element to uploads list
+  add(event) {
+    const [response, status, xhr] = event.detail
+    this.uploadsTarget.insertAdjacentHTML("beforeEnd", response.html)
+  }
+
+  // remove form element
+  // method of removal can be set by data-pshr--uploads-on-remove
+  // default: "delete"
+  remove(event) {
+    console.log(this.onRemove)
+    if (this.onRemove == "reload") {
+      this.reload(event)
+    } else {
+      this.delete(event)
+    }
+  }
+
+  // reload form element
+  // replace target form that triggered the event
+  // with response html form
+  reload(event) {
+    const [response, status, xhr] = event.detail
+    const form = event.target.closest("form")
+    form.outerHTML = response.html
+  }
+
+  // delete form element
+  delete(event) {
+    event.stopPropagation()
+    const form = event.target.closest("form")
+    form.parentNode.removeChild(form)
+  }
+
+  initSortable() {
+    const url = this.data.get("url")
+    const csrf = document.querySelector("[name='csrf-token']").content
+
+    this.sortable = new Sortable(this.uploadsTarget, {
+      draggable: "form",
+      animation: 150,
+      onEnd: (event) => {
+        const id = event.item.dataset.id
+        const index = event.newIndex
+
+        // post to sortable url with
+        // id and new index of dragged item
+        fetch(`${url}/${id}/sort`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": csrf
+          },
+          body: JSON.stringify({ index: index })
+        })
+      }
+    })
+  }
+
+  // get onRemove method from data attribute
+  // default: delete
+  get onRemove() {
+    return this.data.get("onRemove") == "reload" ? "reload" : "delete"
+  }
+}
